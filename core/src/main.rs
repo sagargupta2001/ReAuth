3e69e499b6c77ff732f45c0c41070dff3656ff14
nama.s.gupta@globallogic.com <nama.s.gupta@globallogic.com>
2025-09-07 14:17:29 +0530

    Removed manual migration setup.

--- core/src/main.rs
+++ core/src/main.rs
@@ -8,7 +8,6 @@ use once_cell::sync::Lazy;
 use tracing::info;
 use crate::banner::print_banner;
 use crate::database::init_db;
-use crate::database::migrate::run_migrations;
 use crate::server::start_server;
 use crate::logging::LOGGER;
 use crate::status::log_system_status;
@@ -25,9 +24,6 @@ async fn main() -> anyhow::Result<()> {
     let db = init_db().await?;
 
     log_system_status("http://localhost:3000", Some("http://localhost:5173"), "Up & Running");
-
-    info!("Running migrations...");
-    run_migrations(&db).await?;
 
     info!("Starting server...");
     start_server(db).await?;
