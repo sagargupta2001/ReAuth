c466c3c530a2877940521757235fb8edc6efdbc4
nama.s.gupta@globallogic.com <nama.s.gupta@globallogic.com>
2025-11-26 14:26:53 +0530

    Fixed pagination layout in data-table.

--- ui/src/shared/ui/data-table/pagination.tsx
+++ ui/src/shared/ui/data-table/pagination.tsx
@@ -29,30 +29,25 @@ export function DataTablePagination<TData>({ table, className }: DataTablePagina
       )}
       style={{ overflowClipMargin: 1 }}
     >
-      <div className="flex w-full items-center justify-between">
-        <div className="flex w-[100px] items-center justify-center text-sm font-medium @2xl/content:hidden">
-          Page {currentPage} of {totalPages}
-        </div>
-        <div className="flex items-center gap-2 @max-2xl/content:flex-row-reverse">
-          <Select
-            value={`${table.getState().pagination.pageSize}`}
-            onValueChange={(value) => {
-              table.setPageSize(Number(value))
-            }}
-          >
-            <SelectTrigger className="h-8 w-[70px]">
-              <SelectValue placeholder={table.getState().pagination.pageSize} />
-            </SelectTrigger>
-            <SelectContent side="top">
-              {[10, 20, 30, 40, 50].map((pageSize) => (
-                <SelectItem key={pageSize} value={`${pageSize}`}>
-                  {pageSize}
-                </SelectItem>
-              ))}
-            </SelectContent>
-          </Select>
-          <p className="hidden text-sm font-medium sm:block">Rows per page</p>
-        </div>
+      <div className="flex items-center gap-2 @max-2xl/content:flex-row-reverse">
+        <Select
+          value={`${table.getState().pagination.pageSize}`}
+          onValueChange={(value) => {
+            table.setPageSize(Number(value))
+          }}
+        >
+          <SelectTrigger className="h-8 w-[70px]">
+            <SelectValue placeholder={table.getState().pagination.pageSize} />
+          </SelectTrigger>
+          <SelectContent side="top">
+            {[10, 20, 30, 40, 50].map((pageSize) => (
+              <SelectItem key={pageSize} value={`${pageSize}`}>
+                {pageSize}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </Select>
+        <p className="hidden text-sm font-medium sm:block">Rows per page</p>
       </div>
 
       <div className="flex items-center sm:space-x-6 lg:space-x-8">
